68902552233f900d51b20a886c4c522966db59f8
Ygor Canalli <ygor.canalli@gmail.com>
2018-07-12 15:52:11 -0300

    testing bihyperbolic vgg on mnist

--- mnist.py
+++ mnist.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import torch.nn.init as init
 from torchvision import datasets, transforms
 from torch.autograd import Variable
-from custom import AdaptativeBiHyperbolicMLPNet, MLPNet
+from models import AdaptativeBiHyperbolicMLPNet, MLPNet
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
@@ -50,11 +50,12 @@ test_loader = torch.utils.data.DataLoader(
                    ])),
     batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-# model = AdaptativeBiHyperbolicMLPNet(28*28, 10, [800,500,800,80,800])
-model = MLPNet(28*28, 10, [800,500,800,80,800])
+model = AdaptativeBiHyperbolicMLPNet(28*28, 10, [1024,512,512,1024])
+# model = MLPNet(28*28, 10, [512,1024,1024,512])
 if args.cuda:
     model.cuda()
 
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 def train(epoch):
@@ -65,7 +66,7 @@ def train(epoch):
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -82,7 +83,7 @@ def test():
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data, requires_grad=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+        test_loss += criterion(output, target).data[0] 
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
 
