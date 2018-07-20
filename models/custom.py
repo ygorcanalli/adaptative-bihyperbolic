correcting activation overflow on assymetric bihyperbolic

models/custom.py
@@ -12,11 +12,13 @@ def _unidimensional_xavier_normal(tensor, fan_in, fan_out, gain=1):
 
 def _bi_hyperbolic(tensor, lmbda, tau_1, tau_2, range='tanh'):
     if range is 'sigmoid':
-        return 0.5 * ((torch.sqrt((2 * lmbda * tensor + 1)**2 + 4 * tau_1**2) -
-            torch.sqrt((1 - 2 * lmbda * tensor)**2 + 4 * tau_2**2)) + 1)
+        return torch.max(0,torch.min(0.5 * (
+            (torch.sqrt((2 * lmbda * tensor + 1)**2 + 4 * tau_1**2) - 
+            torch.sqrt((1 - 2 * lmbda * tensor)**2 + 4 * tau_2**2)) + 1),1))
     else:
-        return (torch.sqrt(1/16*(4 * lmbda * tensor + 1)**2 + tau_1**2) -
-            torch.sqrt(1/16*(4 * lmbda * tensor - 1)**2 + tau_2**2))
+        return torch.max(-1,torch.min(
+            (torch.sqrt(1/16*(4 * lmbda * tensor + 1)**2 + tau_1**2) -
+            torch.sqrt(1/16*(4 * lmbda * tensor - 1)**2 + tau_2**2),1))
 
 class MLPNet(nn.Module):
     def __init__(self, in_size, out_size, hidden_sizes):